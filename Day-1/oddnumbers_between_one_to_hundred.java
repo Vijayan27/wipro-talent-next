4e7ef670b4fcddbe4f25f2eef7e6345c1c827080
Vijayan27 <91115378+Vijayan27@users.noreply.github.com>
2023-02-09 11:16:04 +0530

    Add files via upload

---
+++ Day-1/oddnumbers_between_one_to_hundred.java
@@ -0,0 +1,16 @@
+package dinesh;
+import java.util.Scanner;
+
+public class oddnumbers_between_one_to_hundred{
+
+	public static void main(String[] args) {
+		for(int i=1;i<101;i=i+2)
+		{
+			System.out.print(i+" ");
+		}
+				
+			
+
+	}
+
+}
