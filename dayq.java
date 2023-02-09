b7570ca28e5922e6b6740fc974f81657f6d5786e
Vijayan27 <91115378+Vijayan27@users.noreply.github.com>
2023-02-09 11:05:19 +0530

    Add files via upload

---
+++ dayq.java
@@ -0,0 +1,16 @@
+package dinesh;
+import java.util.Scanner;
+
+public class Backward_skipping_two{
+
+	public static void main(String[] args) {
+		for(int i=100;i>=0;i=i-3)
+		{
+			System.out.print(i+" ");
+		}
+				
+			
+
+	}
+
+}
