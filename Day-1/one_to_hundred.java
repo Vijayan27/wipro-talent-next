e4ce5d3833585b640bb63e75ee7b6d3a29c2fa48
Vijayan27 <91115378+Vijayan27@users.noreply.github.com>
2023-02-09 11:17:05 +0530

    Add files via upload

---
+++ Day-1/one_to_hundred.java
@@ -0,0 +1,16 @@
+package dinesh;
+import java.util.Scanner;
+
+public class one_to_hundred{
+
+	public static void main(String[] args) {
+		for(int i=0;i<101;i++)
+		{
+			System.out.print(i+" ");
+		}
+				
+			
+
+	}
+
+}
