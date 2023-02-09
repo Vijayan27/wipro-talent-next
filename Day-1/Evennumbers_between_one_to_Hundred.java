4cd4e1bfd2bc068d92deb84a848d0a507b6ef0e4
Vijayan27 <91115378+Vijayan27@users.noreply.github.com>
2023-02-09 11:10:18 +0530

    Add files via upload

---
+++ Day-1/Evennumbers_between_one_to_Hundred.java
@@ -0,0 +1,16 @@
+package dinesh;
+import java.util.Scanner;
+
+public class Evennumbers_between_one_to_Hundred{
+
+	public static void main(String[] args) {
+		for(int i=0;i<101;i=i+2)
+		{
+			System.out.print(i+" ");
+		}
+				
+			
+
+	}
+
+}
